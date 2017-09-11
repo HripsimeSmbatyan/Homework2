4feb6133d60bf26110898243f140c7978b951f05
HripsimeSmbatyan <31777416+HripsimeSmbatyan@users.noreply.github.com>
2017-09-11 15:27:21 +0400

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,15 @@
+alert('Attention! You are about to grade me 100');
+
+const Num1= 11;
+const Num2= 10;
+console.log (Num1 * Num2);
+console.log ('Hripisime ' + 'Smbatyan');
+
+var age= 16;
+console.log (age * 2);
+var MyGrade = 100;
+
+
+
+if(MyGrade === 100);
+	console.log("I am done");
